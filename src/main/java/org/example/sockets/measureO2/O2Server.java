156cd2771ea3139702e90aa8d2f205e67fd389be
محمد احمد <muhammad.ahmad.github@gmail.com>
2025-12-12 20:47:06 +0500

    fix(socket): concurrency issues in O2Server file

--- src/main/java/org/example/sockets/measureO2/O2Server.java
+++ src/main/java/org/example/sockets/measureO2/O2Server.java
@@ -1,24 +1,43 @@
 package org.example.sockets.measureO2;
+
 import java.io.*;
 import java.net.*;
 
 public class O2Server {
-   public static void main(String[] args){
-       try{
-           ServerSocket serverSocket = new ServerSocket(4444);
-           System.out.println("Server has started!");
-           Socket s = serverSocket.accept();
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(4444);
+            System.out.println("Server has started!");
 
-           //in
-           InputStream in = s.getInputStream();
-           BufferedReader i = new BufferedReader(new InputStreamReader(in));
+            while (true) {
+                Socket s = serverSocket.accept();
+                Thread t = new Thread(new Runnable() {
+                    public void run() {
+                        try {
+                            //in
+                            InputStream in = s.getInputStream();
+                            BufferedReader i = new BufferedReader(new InputStreamReader(in));
 
-           String str;
-           while((str=i.readLine())!=null){
-            int value = Integer.parseInt(str);
-            if(value<80) System.out.println("Patient has Oxygen level less than 80");
-           }
-           
-       }catch(IOException e){}
-   }
+                            String str;
+                            while ((str = i.readLine()) != null) {
+                                int value = Integer.parseInt(str);
+                                if (value < 80) System.out.println("Patient has Oxygen level less than 80");
+                            }
+                        } catch (IOException e) {
+                        } finally {
+                            try {
+                                s.close();
+                            } catch (IOException e) {
+                            }
+                        }
+                    }
+                }
+                );
+
+                t.start();
+            }
+
+        } catch (IOException e) {
+        }
+    }
 }
